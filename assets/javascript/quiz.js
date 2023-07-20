d3e30d4be73d410078ceaa4a7670f873991381b0
Danny Wong <91531924+Zin-Act@users.noreply.github.com>
2023-07-20 11:19:38 +0000

    javascript

---
+++ assets/javascript/quiz.js
@@ -0,0 +1,10 @@
+const easy = document.getElementById(easy);
+const hard = document.getElementById(hard);
+const quiz = document.getElementById(quiz);
+
+easy.addEventListener("click", easyMode);
+
+function easyMode() {
+    quiz.src = example;
+    console.log("testing");
+}
