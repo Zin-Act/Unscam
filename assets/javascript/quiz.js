4e970038a622313b0ea009ee575750738c76495c
Danny Wong <91531924+Zin-Act@users.noreply.github.com>
2023-07-29 14:23:56 +1000

    quiz link

--- assets/javascript/quiz.js
+++ assets/javascript/quiz.js
@@ -1,10 +1,6 @@
-const easy = document.getElementById("easy");
-const hard = document.getElementById("hard");
-const quiz = document.getElementById("quiz");
-
 easy.addEventListener("click", easyMode);
 
 function easyMode() {
     document.getElementById("quiz").src = "https://www.westpac.com.au/security/protect-yourself-and-your-business/scams-quiz/scam-quiz/#/scamQuiz";
-    console.log("testing");
+    document.getElementById("link").href = "https://www.consumer.vic.gov.au/scam-quiz"
 }
