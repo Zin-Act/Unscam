aa5c10a22a85fed23a2a737ff71c1c662ea8c02b
Danny Wong <91531924+Zin-Act@users.noreply.github.com>
2023-07-29 14:25:35 +1000

    testing changes

--- assets/javascript/quiz.js
+++ assets/javascript/quiz.js
@@ -1,6 +1,7 @@
-easy.addEventListener("click", easyMode);
+easy.addEventListener("click", easyMode)
+document.getElementById("link").href = "https://www.consumer.vic.gov.au/scam-quiz"
 
 function easyMode() {
-    document.getElementById("quiz").src = "https://www.westpac.com.au/security/protect-yourself-and-your-business/scams-quiz/scam-quiz/#/scamQuiz";
-    document.getElementById("link").href = "https://www.consumer.vic.gov.au/scam-quiz"
+    document.getElementById("quiz").src = "https://www.westpac.com.au/security/protect-yourself-and-your-business/scams-quiz/scam-quiz/#/scamQuiz"
+    document.getElementById("link").href = "https://www.westpac.com.au/security/protect-yourself-and-your-business/scams-quiz/scam-quiz/#/scamQuiz"
 }
