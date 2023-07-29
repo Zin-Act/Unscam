8ac4f82db585d16f169945bc860321ac50661dce
Danny Wong <91531924+Zin-Act@users.noreply.github.com>
2023-07-29 14:14:37 +1000

    new quiz

--- assets/javascript/quiz.js
+++ assets/javascript/quiz.js
@@ -5,6 +5,6 @@ const quiz = document.getElementById("quiz");
 easy.addEventListener("click", easyMode);
 
 function easyMode() {
-    document.getElementById("quiz").src = "https://www.abc.net.au/news/2022-10-17/would-you-fall-for-these-scams-take-the-test/101500782?utm_campaign=abc_news_web&utm_content=link&utm_medium=content_shared&utm_source=abc_news_web";
+    document.getElementById("quiz").src = "https://www.ftc.gov/business-guidance/small-businesses/cybersecurity/quiz/tech-support-scam";
     console.log("testing");
 }
