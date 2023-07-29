a7370f4018f33f760ff8f87ce6ced74dff7d474b
Danny Wong <91531924+Zin-Act@users.noreply.github.com>
2023-07-29 14:36:21 +1000

    hard mode button

--- assets/javascript/quiz.js
+++ assets/javascript/quiz.js
@@ -1,7 +1,13 @@
-easy.addEventListener("click", easyMode)
+document.getElementById("easy").addEventListener("click", easyMode)
+document.getElementById("hard").addEventListener("click", hardMode)
 document.getElementById("link").href = "https://www.consumer.vic.gov.au/scam-quiz"
 
 function easyMode() {
     document.getElementById("quiz").src = "https://www.westpac.com.au/security/protect-yourself-and-your-business/scams-quiz/scam-quiz/#/scamQuiz"
     document.getElementById("link").href = "https://www.westpac.com.au/security/protect-yourself-and-your-business/scams-quiz/scam-quiz/#/scamQuiz"
+}
+
+function hardMode() {
+    document.getElementById("quiz").src = "https://www.consumer.vic.gov.au/scam-quiz"
+    document.getElementById("link").href = "https://www.consumer.vic.gov.au/scam-quiz"
 }
