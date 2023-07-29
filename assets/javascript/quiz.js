5f38adae7a4b54ba1fb737311c1af8d86e2f8467
Danny Wong <91531924+Zin-Act@users.noreply.github.com>
2023-07-29 13:54:24 +1000

    new website

--- assets/javascript/quiz.js
+++ assets/javascript/quiz.js
@@ -5,6 +5,6 @@ const quiz = document.getElementById("quiz");
 easy.addEventListener("click", easyMode);
 
 function easyMode() {
-    document.getElementById("quiz").src = "https://www.scamwatch.gov.au/research-and-resources/tools-resources/online-resources/spot-the-scam-quiz";
+    document.getElementById("quiz").src = "https://www.abc.net.au/news/2022-10-17/would-you-fall-for-these-scams-take-the-test/101500782?utm_campaign=abc_news_web&utm_content=link&utm_medium=content_shared&utm_source=abc_news_web";
     console.log("testing");
 }
